73422175dc98f831aebd5e30a5820c589390500c
VlDmVr <shabalovvl@gmail.com>
2018-07-20 16:19:58 +0300

    начало адаптивной верстки

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,8 +1,5 @@
 <?php
 
-/* @var $this \yii\web\View */
-/* @var $content string */
-
 use app\widgets\Alert;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -51,74 +48,73 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
    
-<div class="wrap">
-    
-    <nav class="navbar navbar-default navbar-fixed-top container navbar-main" role="navigation"> 
-        
-        <!--start Панель для админа-->
-        <div class="container for-admin">
-            <ul class="navbar-nav navbar-right nav">
-                <?php if(Yii::$app->user->isGuest): ?>
-                    <li><a href="<?= Url::toRoute(['/login']); ?>">ВОЙТИ</a></li>
-                <?php endif; ?>
-                <?php if(!Yii::$app->user->isGuest): ?>
-                    <li><a href="<?= Url::toRoute(['/admin/default/index']); ?>">ADMIN</a></li>
-                    <li><a href="<?= Url::toRoute(['/login/logout']); ?>">ВЫЙТИ<?= '('.Yii::$app->user->identity->username.')'?></a></li>
-                <?php endif; ?>
-            </ul>
-        </div>
-        <!--end Панель для админа-->
-        
-        <!--start Панель для пользователя-->
-        <?php if(Categories::getCategoriesForNavMenu()): ?>
-        <div class="container for-users">  
-            <ul class="navbar-nav navbar-left nav main-menu">
-                <?php foreach(Categories::getCategoriesForNavMenu() as $key=>$nav):?>
-                    <li><a href="<?= Url::toRoute([$nav['url']])?>"><?= $nav['name']?></a>
-                        <?php if($nav['children']): ?>
-                            <ul class="sub-menu">
-                                <?php foreach($nav['children'] as $k_sub_nuv => $v_sub_nav): ?>
-                                    <li><a href="#"><?= $v_sub_nav['name']; ?></a></li>
+<div class="wrap">   
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-6">
+                <nav class="container navbar-main"> 
+                    <!--start Панель для админа-->
+                    <div class="for-admin">
+                        <ul class="navbar-nav navbar-right nav">
+                            <?php if(Yii::$app->user->isGuest): ?>
+                                <li><a href="<?= Url::toRoute(['/login']); ?>">ВОЙТИ</a></li>
+                            <?php endif; ?>
+                            <?php if(!Yii::$app->user->isGuest): ?>
+                                <li><a href="<?= Url::toRoute(['/admin/default/index']); ?>">ADMIN</a></li>
+                                <li><a href="<?= Url::toRoute(['/login/logout']); ?>">ВЫЙТИ<?= '('.Yii::$app->user->identity->username.')'?></a></li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                    <!--end Панель для админа-->
+
+                    <!--start Панель для пользователя-->
+                    <?php if(Categories::getCategoriesForNavMenu()): ?>
+                        <div class="for-users">
+                            <ul class="main-menu">
+                                <?php foreach(Categories::getCategoriesForNavMenu() as $key=>$nav):?>
+                                    <li><a href="<?= Url::toRoute([$nav['url']])?>"><?= $nav['name']?></a>
+                                        <?php if($nav['children']): ?>
+                                            <ul class="sub-menu">
+                                                <?php foreach($nav['children'] as $k_sub_nuv => $v_sub_nav): ?>
+                                                    <li><a href="#"><?= $v_sub_nav['name']; ?></a></li>
+                                                <?php endforeach; ?>
+                                            </ul>
+                                        <?php endif; ?>
+                                    </li>
                                 <?php endforeach; ?>
                             </ul>
-                        <?php endif; ?>
-                    </li>
-                <?php endforeach; ?>
-            </ul> 
+                        </div>
+
+                    <?php else: ?>
+                        <div class="container for-users no-categories"></div>
+                    <?php endif; ?>
+                    <!--end Панель для пользователя-->
+                </nav>
+            </div>
         </div>
-        <?php else: ?>
-            <div class="container for-users no-categories"></div>
-        <?php endif; ?>
-        <!--end Панель для пользователя-->
-        
-    </nav>
-    
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <!--<div id="main-slider">-->
-            <?= Carousel::widget([
-                'items' => $carousel,
-                'options' => ['class' => 'carousel slide', 'data-interval' => '5000'],
-                'controls' => [
-                '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
-                '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
-                ]
-           ]);?>
-        <!--</div>-->
-        <div id="content_in_content">
-            <?= $content ?>
+        <div class="wrap-content">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <!--<div id="main-slider">-->
+                <?= Carousel::widget([
+                    'items' => $carousel,
+                    'options' => ['class' => 'carousel slide', 'data-interval' => '5000'],
+                    'controls' => [
+                    '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
+                    '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'
+                    ]
+               ]);?>
+            <!--</div>-->
+            <div id="content_in_content">
+                <?= $content ?>
+            </div>
         </div>
+        <footer class="footer">
+                <p class="pull-left">&copy;&nbsp;<?= date('Y') ?>&nbsp;АО «Российские космические системы»</p>
+        </footer>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy;&nbsp;<?= date('Y') ?>&nbsp;АО «Российские космические системы»</p>
-    </div>
-</footer>
-
 <?php $this->endBody() ?>
 </body>
 </html>
--- views/site/index.php
+++ views/site/index.php
@@ -1,47 +1,32 @@
-<h1>Главная</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Pellentesque rutrum quam risus, et interdum leo pulvinar sed. 
-    Maecenas blandit lorem sit amet ligula laoreet, ut suscipit lorem vehicula. 
-    Nullam nec tristique nisi, eget commodo justo. 
-    Proin aliquam, elit id viverra pellentesque, ante arcu sagittis elit, rhoncus vestibulum orci tortor at lorem. 
-    Nam at nunc tincidunt, accumsan orci quis, faucibus nunc. 
-    Cras maximus ante nec arcu pretium lobortis. In ultricies magna quis vestibulum sagittis.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Maecenas finibus faucibus lacus ac viverra. Phasellus placerat dui in aliquet faucibus. Ut congue magna sed blandit pulvinar. 
-    Aliquam dignissim elit in commodo iaculis. Aliquam pretium tincidunt leo quis luctus.</p>
-<p>Donec vestibulum nunc quis ex fringilla posuere. 
-    Nulla sed leo molestie purus consequat iaculis. 
-    Etiam eget ante nisi. Sed eros felis, dictum vel condimentum a, commodo ut justo. 
-    Aenean aliquet pellentesque pellentesque. Nunc fermentum felis vehicula diam dapibus pharetra. 
-    Proin euismod justo nec dolor tristique tincidunt. Donec viverra sed enim id facilisis.
-    Nam mattis molestie odio quis convallis. Nam placerat orci justo, et commodo diam fringilla a.
-    Nam ultricies condimentum arcu sed pharetra. Integer vitae magna fermentum, pretium erat eu, suscipit justo. 
-    Integer vitae malesuada tellus. Nullam molestie sem sed urna congue pharetra. Phasellus consequat tristique nisl in suscipit.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Pellentesque rutrum quam risus, et interdum leo pulvinar sed. 
-    Maecenas blandit lorem sit amet ligula laoreet, ut suscipit lorem vehicula. 
-    Nullam nec tristique nisi, eget commodo justo. 
-    Proin aliquam, elit id viverra pellentesque, ante arcu sagittis elit, rhoncus vestibulum orci tortor at lorem. 
-    Nam at nunc tincidunt, accumsan orci quis, faucibus nunc. 
-    Cras maximus ante nec arcu pretium lobortis. In ultricies magna quis vestibulum sagittis.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Maecenas finibus faucibus lacus ac viverra. Phasellus placerat dui in aliquet faucibus. Ut congue magna sed blandit pulvinar. 
-    Aliquam dignissim elit in commodo iaculis. Aliquam pretium tincidunt leo quis luctus.</p>
-<p>Donec vestibulum nunc quis ex fringilla posuere. 
-    Nulla sed leo molestie purus consequat iaculis. 
-    Etiam eget ante nisi. Sed eros felis, dictum vel condimentum a, commodo ut justo. 
-    Aenean aliquet pellentesque pellentesque. Nunc fermentum felis vehicula diam dapibus pharetra. 
-    Proin euismod justo nec dolor tristique tincidunt. Donec viverra sed enim id facilisis.
-    Nam mattis molestie odio quis convallis. Nam placerat orci justo, et commodo diam fringilla a.
-    Nam ultricies condimentum arcu sed pharetra. Integer vitae magna fermentum, pretium erat eu, suscipit justo. 
-    Integer vitae malesuada tellus. Nullam molestie sem sed urna congue pharetra. Phasellus consequat tristique nisl in suscipit.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Pellentesque rutrum quam risus, et interdum leo pulvinar sed. 
-    Maecenas blandit lorem sit amet ligula laoreet, ut suscipit lorem vehicula. 
-    Nullam nec tristique nisi, eget commodo justo. 
-    Proin aliquam, elit id viverra pellentesque, ante arcu sagittis elit, rhoncus vestibulum orci tortor at lorem. 
-    Nam at nunc tincidunt, accumsan orci quis, faucibus nunc. 
-    Cras maximus ante nec arcu pretium lobortis. In ultricies magna quis vestibulum sagittis.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Maecenas finibus faucibus lacus ac viverra. Phasellus placerat dui in aliquet faucibus. Ut congue magna sed blandit pulvinar. 
-    Aliquam dignissim elit in commodo iaculis. Aliquam pretium tincidunt leo quis luctus.</p>
+<div class="row">
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+    <div class="col-lg-6 main-padding">
+        <div class="main-image">
+            <a href="#"><img src="uploads/RSS_1043_1_1531740938.jpg"></a>
+        </div>
+    </div>
+</div>
